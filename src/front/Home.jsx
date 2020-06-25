9ca4c3eda2af7ca60f0583daa171632dbf152658
Malik HARRIZ <malik.h@webdevpro.net>
2020-06-25 15:15:17 +0200

    correction page d'accueil

--- src/front/Home.jsx
+++ src/front/Home.jsx
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
-
+import { Link } from "react-router-dom";
 class Home extends Component {
-    state = {  }
-    render() { 
-        return ( <h1>Bienvenue sur l'accueil</h1> );
+    state = {
+        contenu: [
+            { id: 1, titre: "article 1", content: "lorem ipsum", url: "https://via.placeholder.com/400x200" },
+            { id: 2, titre: "article 2", content: "lorem ipsum", url: "https://via.placeholder.com/400x200" },
+            { id: 3, titre: "article 3", content: "lorem ipsum", url: "https://via.placeholder.com/400x200" },
+            { id: 4, titre: "article 4", content: "lorem ipsum", url: "https://via.placeholder.com/400x200" },
+        ]
+    }
+    render() {
+        return (
+            <div>
+                <h1>Bienvenue sur l'accueil</h1>
+                <section className="row">
+                    {this.state.contenu.map(
+                        item =>
+                            <article className="col">
+                                <div className="card px-2">
+                                    <h2>{item.titre}</h2>
+                                    <img src={item.url} alt="" className="img-fluid" />
+                                    <p>{item.content}</p>
+                                    <p><Link to={"/articles/" + item.id} className="btn btn-info">Lire la suite</Link></p>
+                                </div>
+
+                            </article>
+                    )}
+                </section>
+            </div>
+        );
     }
 }
- 
+
 export default Home;
