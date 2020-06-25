3139c3353b7850beb03fce6e5af271388213d90c
Malik HARRIZ <malik.h@webdevpro.net>
2020-06-25 12:33:51 +0200

    navigation programmatique

--- src/front/Post.jsx
+++ src/front/Post.jsx
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 
 class Post extends Component {
     state = {}
+
+    retourArticles() {
+        this.props.history.push("/articles");
+        // react-router-dom
+        // bien respecter les routes mentionnées dans le fichier App.js
+    }
+
     render() {
         return (
             <div>
                 <h1>article numéro {this.props.match.params.id}</h1>
                 <p>lorem ipsum</p>
+                <hr />
+                <button className="btn btn-info" onClick={this.retourArticles.bind(this)}>Retour à la liste des articles</button>
             </div>);
     }
 }
