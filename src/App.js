042a8c3b4c49fff031a7ba1b31e23869fc707958
Malik HARRIZ <malik.h@webdevpro.net>
2020-06-25 14:40:59 +0200

    composant switch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./Navbar";
 
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Home from "./front/Home";
 import Articles from "./front/Articles";
 import Contact from "./front/Contact";
@@ -16,12 +16,14 @@ function App() {
     <div className="container">
       <NavBar />
       <div>
-        <Route path="/articles/:id" component={Post} />
-        <Route path="/articles" exact component={Articles} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/not-found" component={NotFound} />
-        <Route path="/" exact component={Home} />
-        <Redirect to="/not-found" />
+        <Switch>
+          <Route path="/articles/:id" component={Post} />
+          <Route path="/articles" exact component={Articles} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/not-found" component={NotFound} />
+          <Route path="/" exact component={Home} />
+          <Redirect to="/not-found" />
+        </Switch>
       </div>
     </div>
   );
