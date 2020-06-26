04da9788331b9c84183714c6431698cfb6a03b4d
Malik HARRIZ <malik.h@webdevpro.net>
2020-06-26 14:08:33 +0200

    correction formulaire de commentaire dans composant Post

--- src/front/Post.jsx
+++ src/front/Post.jsx
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 import { findArticleById } from "../services/ArticlesFictifs";
 class Post extends Component {
     state = {
-        contenu: {}
+        contenu: {},
+        form: {
+            commentaire: ""
+        },
+        error: false,
+        success: false
     }
     retourArticles() {
         this.props.history.push("/articles");
@@ -16,6 +21,33 @@ class Post extends Component {
         });
     }
 
+    traiterCommentaire(e) {
+        e.preventDefault();
+        const form = { ...this.state.form };
+
+        if (form.commentaire == "") {
+            // le champ du formulaire est vide 
+            this.setState({
+                error: true, // afficher le message d'erreur
+                success: false // masquer le message de success
+            })
+        }
+        else {
+            this.setState({
+                error: false, // masquer le message  d'erreur
+                success: true // afficher le message de success
+            })
+        }
+    }
+
+    change(e) {
+        const form = { ...this.state.form };
+        form[e.target.name] = e.target.value;
+        this.setState({
+            form: form
+        });
+    }
+
 
     render() {
         return (
@@ -24,7 +56,29 @@ class Post extends Component {
                 <img src={this.state.contenu.url + this.state.contenu.id} alt="" />
                 <p>{this.state.contenu.content}</p>
                 <hr />
-                <button className="btn btn-info" onClick={this.retourArticles.bind(this)}>Retour à la liste des articles</button>
+                <h2>Laisser un commentaire :</h2>
+                <form onSubmit={this.traiterCommentaire.bind(this)}>
+                    <div className="form-group">
+                        <label htmlFor="commentaire">Commentaire</label>
+                        <textarea
+                            name="commentaire"
+                            id="commentaire"
+                            rows="5"
+                            className="form-control"
+                            placeholder="laisser un commentaire"
+                            value={this.state.form.commentaire}
+                            onChange={this.change.bind(this)}
+                        ></textarea>
+                    </div>
+                    <div className="form-group d-flex">
+                        <input type="submit" className="btn btn-outline-success" />
+                        <button className="btn btn-info ml-auto" onClick={this.retourArticles.bind(this)}>Retour à la liste des articles</button>
+                    </div>
+                </form>
+
+                {this.state.error && <p className="alert alert-danger">Veuillez compléter le champ commentaire</p>}
+                {this.state.success && <p className="alert alert-success">Merci pour votre commentaire</p>}
+
             </div>);
     }
 }
